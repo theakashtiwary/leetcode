6a30de17cc1a68998dfa7882d55ac0faeeed82dd
Akash Tiwary <134575604+theakashtiwary@users.noreply.github.com>
2025-03-02 22:01:59 +0530

    Time: 61 ms (30.70%) | Memory: 28.9 MB (84.83%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,31 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin() , nums.end());     //Sorted Array
-        if(nums.size() < 3){                // Base Case 1
-            return {};
-        }
-        if(nums[0] > 0){                    // Base Case 2
-            return {};
-        }
-        unordered_map<int , int> hashMap;
-        for(int i = 0 ; i < nums.size() ; ++i){     //Hashing of Indices
-            hashMap[nums[i]] = i;
-        }
-        vector<vector<int>> answer;
-        for(int i = 0 ; i < nums.size() - 2 ; ++i){     //Traversing the array to fix the number.
-            if(nums[i] > 0){     //If number fixed is +ve, stop there because we can't make it zero by searching after it.
-                break;
-            }
-            for(int j = i + 1 ; j < nums.size() - 1 ; ++j){     //Fixing another number after first number
-                int required = -1*(nums[i] + nums[j]);    //To make sum 0, we would require the -ve sum of both fixed numbers.
-                if(hashMap.count(required) && hashMap.find(required)->second > j){ //If it exists in hashmap and its last occurrence index > 2nd fixed index, we found our triplet.
-                    answer.push_back({nums[i] , nums[j] , required});
+        vector<vector<int>> ans;
+        int n = nums.size();
+        int target = 0;
+        
+        sort(nums.begin(), nums.end());  // Sort the array
+        
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate elements for `i`
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int l = i + 1;
+            int r = n - 1;
+            
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                
+                if (sum == target) {
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    
+                    // Move the left pointer and skip duplicates
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    // Move the right pointer and skip duplicates
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+                    
+                    // Move both pointers after processing
+                    l++;
+                    r--;
+                } 
+                else if (sum < target) {
+                    l++;  // Move left pointer to increase the sum
+                } 
+                else {
+                    r--;  // Move right pointer to decrease the sum
                 }
-                j = hashMap.find(nums[j])->second; //Update j to last occurence of 2nd fixed number to avoid duplicate triplets.
             }
-            i = hashMap.find(nums[i])->second;     //Update i to last occurence of 1st fixed number to avoid duplicate triplets.
         }
-        return answer;  //Return answer vector.
+        
+        return ans;
     }
 };
