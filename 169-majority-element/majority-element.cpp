b42cef887d027494e3113aa0e0b990ed7e8a9447
Akash Tiwary <134575604+theakashtiwary@users.noreply.github.com>
2026-03-16 00:21:15 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (7.85%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-
-
-
-
     int majorityElement(vector<int>& nums) {
 
-      map<int,int>m;
-      for(int i=0;i<nums.size();i++){
-        m[nums[i]]++;
-      }
-      int maxf=m[nums[0]];
-      int key=nums[0];
-      for(auto it:m){
-        if(it.second>maxf){
-            maxf=it.second;
-            key=it.first;
+        map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;// yaha frrequency store kra di
         }
-      }
 
-
-return key;
+        for (auto it : m) {
+            if (it.second > nums.size() / 2) { // simple si baat hai jis element ki freq n/2 se jada hai usko return kr do
+                return it.first;
+            }
+        }
+        return -1;
     }
 };
